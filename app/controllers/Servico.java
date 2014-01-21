76ddfa1c0ba8786c3d370c4a76e7ed7684930b1b
linuxsoares <linux.soares@gmail.com>
2014-01-20 22:21:11 -0200

    Iniciando Projeto

---
+++ app/controllers/Servico.java
@@ -0,0 +1,43 @@
+package controllers;
+
+/**
+ * Created by gilmar on 20/01/14.
+ */
+
+import play.*;
+import play.mvc.*;
+import models.*;
+import play.libs.Json;
+
+public class Servico extends Controller {
+
+    public static Result adicionar(){
+
+        ObjectNode response = Json.newObject();
+
+        Tarefa tarefa = new Tarefa();
+        tarefa.id = (long)tarefas.values().size();
+        tarefa.descricao = request().body().asMultipartFormData().asFormUrlEncoded().get("descricao")[0];
+        tarefa.titulo = request().body().asMultipartFormData().asFormUrlEncoded().get("titulo")[0];
+        tarefa.completada = false;
+
+        tarefas.put(tarefa.id, tarefa);
+
+        response.put("id", tarefa.id);
+        response.put("resposta", "Tarefa Adicionada com Sucesso.");
+
+        return ok(response);
+    }
+
+    public static Result excluir(Long id){
+        return null;
+    }
+
+    public static Result completar(Long id){
+        return null;
+    }
+
+    public static Result listar(){
+        return null;
+    }
+}
---
+++ app/models/Tarefa.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by gilmar on 20/01/14.
+ */
+public class Tarefa {
+    public Long id;
+    public String titulo;
+    public String descricao;
+    public boolean completada;
+}
